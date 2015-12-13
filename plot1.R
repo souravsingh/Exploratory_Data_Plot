68a43b7c3f285dc097a1c7b6a5b72326c16b8c64
Sourav Singh <souravsingh@users.noreply.github.com>
2015-12-13 23:49:09 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,11 +1,12 @@
 rm(list=ls())
+
 #reading data into R:
 cons<- read.table("household_power_consumption.txt", sep=";",nrows= 2075260, header=TRUE, quote= "", strip.white=TRUE, stringsAsFactors = FALSE, na.strings= "?")
 
-# Subsetting the full data to obtain the data related to two days: 
+# Subsetting the full data to obtain the data which is related to two days: 
 sub<- subset(cons, (cons$Date == "1/2/2007" | cons$Date== "2/2/2007")) 
 
-# Changing the class of Date variable from character to Date: 
+# Changing class of Date variable from character to Date: 
 sub$Date <- as.Date(sub$Date, format = "%d/%m/%Y")
 
 # Creating the plot3:
